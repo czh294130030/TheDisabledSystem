8e9dfecf8c84b853c9bdaa329fa841a71f180cf6
czh294130030 <294130030@qq.com>
2014-10-02 23:48:30 +0800

    add table d_adminType

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -24,6 +24,18 @@ go
 ---------数据表操作-------------
 use TheDisabledDB  
 go
+---------管理类型表-------------
+if exists (select * from sysobjects where name='d_adminType')  
+	drop table d_adminType
+go  
+create table d_adminType  
+(  
+	id int primary key identity(1,1),
+	name nvarchar(50)
+)
+insert into d_adminType(name)values('超级管理员');
+insert into d_adminType(name)values('管理员');
+--select * from d_adminType;
 ---------管理员表-------------
 if exists (select * from sysobjects where name='d_admin')  
 	drop table d_admin
@@ -32,5 +44,9 @@ create table d_admin
 (  
 	id int primary key identity(1,1),
 	username nvarchar(50),
-	password nvarchar(50)
+	password nvarchar(50),
+	typeId int
 ) 
+insert into d_admin(username,password,typeId) values('admin','123456',1);
+insert into d_admin(username,password,typeId) values('chad','123456',2);
+--select * from d_admin;
